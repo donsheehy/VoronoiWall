29f5b0be5b720de349b5331c8830d876ea48ac46
Don Sheehy <don@Donalds-MacBook-Pro.local>
2019-10-22 13:52:36 -0400

    edited dons_test to look nicer.

--- dons_test.py
+++ dons_test.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d as a3
 import scipy as sp
 
-import matplotlib.colors as colors
 from scipy.spatial import Voronoi
 
 def reflect(points, x = 1, y = 1 , z = 1, dx = 0, dy = 0, dz = 0):
@@ -13,18 +12,16 @@ def reflect(points, x = 1, y = 1 , z = 1, dx = 0, dy = 0, dz = 0):
 def containerize(points, box):
     left = reflect(points, x = -1, dx = box[0][0])
     right = reflect(points, x = -1, dx = 2*box[0][1])
-    points.extend(left + right)
     bottom = reflect(points, y = -1, dy = box[1][0])
     top = reflect(points, y = -1, dy = 2*box[1][1])
-    points.extend(top + bottom)
     front = reflect(points, z = -1, dz = box[2][0])
     back = reflect(points, z = -1, dz = 2 * box[2][1])
-    points.extend(front + back)
+    points.extend(left + right + top + bottom+ front + back)
 
 
 def main():
     # Prepare the points.
-    n = 200
+    n = 500
     # data = [[i/n, 0.01 * i**2 / (n ** 2), i**3/ (n**3)] for i in range(n)]
     data = [sp.rand(3) for i in range(n)]
     containerize(data, [[0, 1], [0, 1], [0, 1]])
@@ -35,10 +32,11 @@ def main():
 
     # Prepare to draw.
     ax = a3.Axes3D(plt.figure())
+    ax.set_axis_off()
     facets = []
 
     # Generate the facets of some of the Voronoi regions.
-    k = set(range(19)) # indices of Voronoi regions to plot.
+    k = set(range(10)) # indices of Voronoi regions to plot.
     for i, pair in enumerate(vor.ridge_points):
         if k & set(pair):
             facets.append([list(vor.vertices[i]) for i in vor.ridge_vertices[i]])
@@ -49,7 +47,7 @@ def main():
 
     # Plot the facets.
     polygons = a3.art3d.Poly3DCollection(facets)
-    polygons.set_alpha(0.5)
+    polygons.set_alpha(0.99)
     polygons.set_edgecolor('k')
     ax.add_collection3d(polygons)
 
