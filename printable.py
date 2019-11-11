ccdc802d3d0d0f3acd908197de7691c7d694159d
Alex Tyner <36014968+alextyner@users.noreply.github.com>
2019-11-11 16:11:10 -0500

    STL conversion works on diagrams with facets having >3 vertices now, but is slower.

--- printable.py
+++ printable.py
@@ -5,9 +5,15 @@ import numpy as np
 def diagramToSTLMesh(diagram):
     vertices = diagram.vertices
 
-    # consider making this act on the facets of bounded regions (some facets might be bounded, but not their region)
-    tris = [[tri.vertices_i for tri in facet.triangles()] for facet in diagram.bounded_facets]
-    tri_facets_i = np.array(tris).squeeze()
+    # collect the facets of bounded regions
+    bounded_region_facets = [y for x in [region.facets for region in diagram.bounded_regions] for y in x]
+
+    # collect the vertices of triangulated facets
+    tri_facets_i = []
+    for facet in bounded_region_facets:
+        for tri in facet.triangles():
+            tri_facets_i.append(tri.vertices_i)
+    tri_facets_i = np.array(tri_facets_i)
 
     m = mesh.Mesh(np.zeros(tri_facets_i.shape[0], dtype=mesh.Mesh.dtype))
     for i, facet_i in enumerate(tri_facets_i):
@@ -31,6 +37,10 @@ def fixOrdering(diagram):
         for region in facet.regions:
             if -1 not in region.vertices_i:
                 input_point = region.point
+        
+        # if this bounded facet was part of an unbounded region
+        if input_point is None:
+            continue
 
         normal_vector = np.cross(facet.vertices[1] - facet.vertices[0], facet.vertices[2] - facet.vertices[1])
         point_direction_vector = facet.vertices[0] - input_point.point
@@ -54,8 +64,12 @@ if __name__ == '__main__':
     from structures import Diagram
 
     points = np.array([[0, 0, 3], [0, 3, 3], [3, 0, 3], [3, 3, 3], [0, 0, 0], [0, 3, 0], [3, 0, 0], [3, 3, 0],
-                       [1, 2, 2], [2, 2, 2], [1, 2, 1], [2, 2, 1], [1, 1, 2], [2, 1, 2], [1, 1, 1], [2, 1, 1]])
-
+                      [1, 2, 2], [2, 2, 2], [1, 2, 1], [2, 2, 1], [1, 1, 2], [2, 1, 2], [1, 1, 1], [2, 1, 1]])
+    points = np.array([[6, 4, 2], [9, 5, 8], [9, 1, 9], [8, 9, 1], [3, 8, 8], [2, 6, 2], [8, 2, 10], [3, 6, 1], [9, 8, 9],
+           [7, 7, 4],
+           [2, 10, 5], [4, 3, 10], [5, 3, 9], [4, 7, 4], [3, 6, 7], [7, 4, 3], [6, 4, 9], [5, 8, 4], [2, 9, 10],
+           [7, 8, 6], [9, 2, 7], [6, 10, 7], [9, 9, 3], [2, 9, 4], [5, 9, 6], [4, 8, 9], [9, 1, 2], [6, 9, 1],
+           [10, 6, 5], [1, 9, 9], [2, 1, 3], [10, 1, 5], [4, 10, 2]])
     # Generate the Diagram data structure
     diagram = Diagram(points)
 
